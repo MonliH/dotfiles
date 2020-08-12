5cb85914f4447897b2f04b5d7f00479cc6bf68ea
Jonathan <30177086+MonliH@users.noreply.github.com>
2020-08-11 21:30:44 -0400

    Remove fading

--- picom/launch.sh
+++ picom/launch.sh
@@ -6,6 +6,6 @@ killall -q picom
 # Wait until the processes have been shut down
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 
-picom &
+picom --experimental-backends &
 
 echo "Picom launched..."
